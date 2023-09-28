3c26319c8d7acd6f064d18847d4bb21c96b3be3e
Ian Martínez <ian9paul@hotmail.com>
2023-09-28 12:38:52 -0600

    update to lex

--- lex.py
+++ lex.py
@@ -1,18 +1,5 @@
 import ply.lex as lex
 
-# List of token names.   This is always required
-tokens = (
-   'NUMBER',
-   'PLUS',
-   'MINUS',
-   'TIMES',
-   'DIVIDE',
-   'LPAREN',
-   'RPAREN',
-   'ID',
-)
-
-'''
 # Reserved words
 reserved = {
    'if' : 'IF',
@@ -20,7 +7,27 @@ reserved = {
    'else' : 'ELSE',
    'while' : 'WHILE',
 }
-'''
+
+# List of token names.   This is always required
+tokens = [
+   'NUMBER',
+   'PLUS',
+   'MINUS',
+   'TIMES',
+   'DIVIDE',
+   'LPAREN',
+   'RPAREN',
+   'EQUAL',
+   'LBRACKET',
+   'RBRACKET',
+   'LBRACE',
+   'RBRACE',
+   'COLON',
+   'COMMA',
+   'GREATERTHAN',
+   'SMALLERTHAN',
+   'ID',
+] + list(reserved.values())
 
 # Regular expression rules for simple tokens
 t_PLUS    = r'\+'
@@ -29,6 +36,15 @@ t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
+t_EQUAL   = r'='
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LBRACE  = r'\{'
+t_RBRACE  = r'\}'
+t_COLON   = r':'
+t_COMMA   = r','
+t_GREATERTHAN = '>'
+t_SMALLERTHAN = '<'
 
 # A regular expression rule with some action code
 def t_NUMBER(t):
@@ -39,8 +55,9 @@ def t_NUMBER(t):
 # Identifiers handler
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    #t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
+
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -59,7 +76,7 @@ def t_error(t):
 lexer = lex.lex()
 
 # Test
-data = "3 + 4 * variable_1 - 2 + els"
+data = "z = [ 3 + 4 ] * variable_1 - 2 + else { Hola }, s: 1,2"
 lexer.input(data)
 
 # Print tokens
